e00a031f136930508a075cdb384f9ded56467389
claudio.chag@hotmail.com <claudio.chag@hotmail.com>
2023-08-31 17:50:36 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MauiCalculadora.MVVM.Views;
+
+namespace MauiCalculadora;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new CalculadoraView();
+	}
+}
---
+++ MVVM/Views/CalculadoraView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiCalculadora.MVVM.Views;
+
+public partial class CalculadoraView : ContentPage
+{
+	public CalculadoraView()
+	{
+		InitializeComponent();
+	}
+}
